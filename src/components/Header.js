469035f097cf66cb9d8f518072df1d0fb2537e16
FaithOzoanieke <faithozoanieke@gmail.com>
2023-02-01 10:15:21 +0100

    updates from google signin

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { BiSearch } from 'react-icons/bi';
 import { AiOutlineShoppingCart, AiOutlineMenu } from 'react-icons/ai';
 // import { signIn, signOut, useSession } from 'next-auth/client';
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 
 function Header() {
@@ -32,7 +32,7 @@ function Header() {
 
         {/* {Right side} */}
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div onClick={signIn} className='link cursor-pointer'>
+          <div onClick={() => signIn("google")} className='link cursor-pointer'>
             <p>Hello Faith Uju</p>
             <p className='font-extrabold md:text-sm'>Account & Lists</p>
           </div>
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.Google_ID,
-      clientSecret: process.env.Google_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
