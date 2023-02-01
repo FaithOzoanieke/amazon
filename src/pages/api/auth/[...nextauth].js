433969c286a63d5c2b4f20e798cdc34abd3e13d6
FaithOzoanieke <faithozoanieke@gmail.com>
2023-02-01 21:57:59 +0100

    updates from .env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,14 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
+console.log(
+  'process.env.NEXT_PUBLIC_GOOGLE_ID',
+  process.env.NEXT_PUBLIC_GOOGLE_ID
+);
+console.log(
+  'process.env.NEXT_PUBLIC_GOOGLE_SECRET',
+  process.env.NEXT_PUBLIC_GOOGLE_SECRET
+);
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
